db4a6f3cba84dd24271b8c356eb9e26a5f5ec4db
Rodrigo Borcat <rodrigo.borcat@movile.com>
2020-07-04 19:30:05 -0300

    add database setup

--- nestjs-task-management/dist/app.module.js
+++ nestjs-task-management/dist/app.module.js
@@ -9,11 +9,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const tasks_module_1 = require("./tasks/tasks.module");
+const typeorm_1 = require("@nestjs/typeorm");
+const typeorm_config_1 = require("./config/typeorm.config");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     common_1.Module({
-        imports: [tasks_module_1.TasksModule],
+        imports: [
+            typeorm_1.TypeOrmModule.forRoot(typeorm_config_1.typeOrmConfig),
+            tasks_module_1.TasksModule
+        ],
     })
 ], AppModule);
 exports.AppModule = AppModule;
